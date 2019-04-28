58faa0c16562389376940f22bcaccef8a231fd0c
masih <masih_hanif@yahoo.com>
2019-04-28 16:50:37 -0700

    change yaw Controller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -30,6 +30,29 @@ class Controller(object):
 
 
     def control(self, current_vel, angular_vel, linear_vel, dbw_enabled):
-        # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
-        return 1., 0., 0.
+    	if dbw_enabled:
+    		self.throttle_controller.reset()
+    		return 0., 0., 0.
+
+    	current_vel = self.vel_lpf.filt(current_vel)
+
+    	steering = self.yaw_controller.get_ste=eering(linear_vel, angular_vel, current_vel)
+
+    	vel_error = linear_vel - current_vel
+    	self.last_vel = current_vel
+
+    	current_time = rospy.get_time()
+    	sample_time = current_time - self.last_time
+    	self.last_time = current_time
+
+    	throttle = self.throttle_controller.step(vel_error, sample_time)
+
+    	if linear_vel == 0 and current_vel < 0.1 :
+    		throttle = 0
+    		brake = 400 # Nm
+    	elif throttle < 0.1 and vel_error < 0:
+    		throttle = 0
+    		decel = max(vel_error, self.decel_limit)
+    		brake = abs(decel)*self.vehicle_mass*self.wheel_radius
+
+        return throttle, brake, steering
